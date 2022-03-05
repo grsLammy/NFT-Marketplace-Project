d73b4bb8103ee9250231474bdab40deea842c56b
grsLammy <grspro98@gmail.com>
2022-03-06 02:59:51 +0530

    Created KryptoHamster.sol contract and defined some metadata also created 2_deploy_contract.js and defined it to deploy our KryptoHamster.sol

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const KryptoHamster = artifacts.require("KryptoHamster");
+
+module.exports = function (deployer) {
+
+    deployer.deploy(KryptoHamster);
+    
+};
---
+++ src/contracts/KryptoHamster.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract KryptoHamster {
+
+    string public name;
+    string public symbol;
+
+    constructor () {
+        name = "Krypto Hammy";
+        symbol = "HAMI";
+    }
+}
