0500deff53ca2442418bda7fdbb9963a898f0667
grsLammy <grspro98@gmail.com>
2022-03-07 20:03:22 +0530

    Transfer functionality + approval functionality added to the ERC721.sol

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -9,10 +9,23 @@ contract ERC721 {
     // Mapping from owner to number of owned tokens
     mapping(address => uint256) private _countOfTokenOwned; 
 
-    event tokenMinted(
-        address indexed mintedFrom,
-        address indexed mintedTo,
-        uint256 indexed mintedTokenId
+    // Mapping from token ids to approved addresses
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId
+    );
+
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool indexed _approved
     );
 
     // Function to return the balance of the owner
@@ -26,10 +39,10 @@ contract ERC721 {
     }
 
     // Function to return the address of the token owner by the tokenId
-    function ownerOf(uint256 tokenId) external view returns (address) {
+    function ownerOf(uint256 _tokenId) external view returns (address) {
 
         // Assigns the mapping of owner address from tokenId to owner variable
-        address owner = _tokenOwner[tokenId];
+        address owner = _tokenOwner[_tokenId];
 
         // Requires that the owner address cannot be zero
         require(owner != address(0), "owner query for non-existent token");
@@ -49,6 +62,55 @@ contract ERC721 {
         return owner != address(0);
     }
 
+    function approve(address _to, uint256 _tokenId) internal {
+
+        address _owner = this.ownerOf(_tokenId);
+
+        require(_to != _owner, "Approval to current owner is not possible");
+        require(msg.sender == _owner || isApprovedForAll(_owner, msg.sender), "ERC721: approve caller is not owner nor approved for all");
+        _tokenApprovals[_tokenId] = _to;
+        
+    }
+
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
+        require(_exists(tokenId), "ERC721: operator query for nonexistent token");
+        address owner = this.ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    }
+
+    function getApproved(uint256 tokenId) internal view returns (address) {
+        require(_exists(tokenId), "ERC721: approved query for nonexistent token");
+
+        return _tokenApprovals[tokenId];
+    }
+
+    function setApprovalForAll(address operator, bool approved) internal {
+        require(operator != msg.sender, "ERC721: approve to caller");
+
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    function isApprovedForAll(address owner, address operator) internal view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    function _transferFrom(address _from, address _to, uint256 _tokenId) internal {
+
+        require(_to != address(0), "ERC721 Transfer to the zero address");
+        require(this.ownerOf(_tokenId) == _from, "The address does not own the token its trying to transfer");
+        _countOfTokenOwned[_from] -= 1;
+        _countOfTokenOwned[_to] += 1;
+        _tokenOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public {
+        require(_isApprovedOrOwner(msg.sender, _tokenId), "The caller is not appproved or is not an owner");
+        _transferFrom(_from, _to, _tokenId);
+    }
+
     function _mintToken(address _to, uint256 tokenId) internal virtual {
 
         // Require that the address is not zero
@@ -63,6 +125,6 @@ contract ERC721 {
         // Adding one to the count for each address that is miniting
         _countOfTokenOwned[_to] += 1;
 
-        emit tokenMinted(address(0), _to, tokenId);
+        emit Transfer(address(0), _to, tokenId);
     } 
 }
--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -16,26 +16,41 @@ contract ERC721Enumerable is ERC721 {
     // Mapping from token ID to index of the owner token list
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
-    //function tokenByIndex(uint256 _index) external view returns (uint256) {}
-    
-    //function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256) {} 
-
-    function totalSupply() public view returns(uint256) {
-        return _allTokens.length;
+    // Add tokens to the _allTokens array and set the position of the token indexes
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
     }
 
+    // Add tokens to the _ownedTokens owner address and set the positon of the owned token indexes 
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length; 
+        _ownedTokens[to].push(tokenId);
+    }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
-        _allTokens.push(tokenId);
+    function tokenByIndex(uint256 _index) external view returns (uint256) {
+        // Make sure that the index is not out of bounds of the total supply
+        require(_index < this.totalSupply(), "global index is out of bounds");
+        return _allTokens[_index];
+    }
+    
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256) {
+        require(_index < this.balanceOf(_owner), "owner index is out of bounds");
+        return _ownedTokens[_owner][_index];
+    } 
+
+    // Return the total supply of the _allToken array
+    function totalSupply() public view returns(uint256) {
+        
+        return _allTokens.length;
     }
 
     function _mintToken(address to, uint256 tokenId) internal override (ERC721) {
 
         super._mintToken(to, tokenId);
 
-        // Add tokens to the owners address
-        // Add all tokens to total supply - to allTokens
-        _addTokensToTotalSupply(tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokenToOwnerEnumeration(to, tokenId);
     }
 
 }
